93aa9210c991a01b6b6289de8a339bf919ad3881
jp <jp@jpls-Inspiron-5480>
2021-03-16 16:16:53 -0300

    excluido

--- LojaFlores/src/main/java/br/edu/ifpb/dac/loja/LojaFloresApplication.java
+++ LojaFlores/src/main/java/br/edu/ifpb/dac/loja/LojaFloresApplication.java
@@ -2,8 +2,6 @@ package br.edu.ifpb.dac.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
--- LojaFlores/src/main/java/br/edu/ifpb/dac/loja/servico/CompraService.java
+++ LojaFlores/src/main/java/br/edu/ifpb/dac/loja/servico/CompraService.java
@@ -1,7 +1,6 @@
 package br.edu.ifpb.dac.loja.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
